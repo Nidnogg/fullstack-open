9c0297927cebc037e29adf67ad9da1cb25a575a1
nidnogg <henriquevt98@gmail.com>
2020-07-08 20:38:57 -0300

    finished unicafe

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -8,51 +8,76 @@ const Header = () => {
   )
 }
 
+const Statistic = (props) => {
+  const {text, stat} = props
+  return (
+    <tr>
+      <td>
+        {text}
+      </td>
+      <td>
+        {stat}
+      </td>
+    </tr>
+  )
+}
+
 const Statistics = (props) => {
   const {stats} = props
   
+  const feedback = () => {
+    if(stats.all > 0) {
+      return (
+        <table>
+          <tbody>
+            <Statistic text={"good"} stat={stats.good} />
+            <Statistic text={"neutral"} stat={stats.neutral} />
+            <Statistic text={"bad"} stat={stats.bad} />
+            <Statistic text={"all"} stat={stats.all} />
+            <Statistic text={"average"} stat={stats.average} />
+            <Statistic text={"positive"} stat={stats.positive}>%</Statistic>
+          </tbody>
+        </table>
+      )
+    } else {
+      return (
+        <p>No feedback given</p>
+      )
+    }
+  }
   return (
     <div>
       <h1>Statistics</h1>
       <div>
-        <p>good {stats.good}</p>
-        <p>neutral {stats.neutral}</p>
-        <p>bad {stats.bad}</p>
-        <p>average {stats.average}</p>
-        <p>positive {stats.positive}%</p>
+       {feedback()}
       </div>
     </div>
   )
 }
 const App = () => {
-  // save clicks of each button to own state
-
-  /*
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  */
+ 
   const [stats, setStats] = useState({
     good: 0,
     neutral: 0,
-    bad: 0, 
+    bad: 0,
+    all: 0, 
     average: 0,
     positive: 0
   })
 
-
   const average = (pos, neutral, bad) => {
-    return (pos + neutral + bad)/3.0
+    return ((pos + neutral + bad)/3.0).toFixed(2)
   }
 
   const positive = (pos, neutral, bad) => {
-    return (pos)*100/(pos + neutral + bad)
+    return ((pos)*100/(pos + neutral + bad)).toFixed(2)
   }
 
   const handleGood = () => {
     const newStats = {
       ...stats,
       good: stats.good+=1,
+      all: stats.all+=1,
       average: average(stats.good+1, stats.neutral, stats.bad),
       positive: positive(stats.good+1, stats.neutral, stats.bad)
     }
@@ -63,6 +88,7 @@ const App = () => {
     const newStats = {
       ...stats,
       neutral: stats.neutral+=1,
+      all: stats.all+=1,
       average: average(stats.good, stats.neutral+1, stats.bad),
       positive: positive(stats.good, stats.neutral+1, stats.bad)
     }
@@ -73,12 +99,12 @@ const App = () => {
     const newStats = {
       ...stats,
       bad: stats.bad+=1,
+      all: stats.all+=1,
       average: average(stats.good, stats.neutral, stats.bad+1),
       positive: positive(stats.good, stats.neutral, stats.bad+1)
     }
     setStats(newStats)
   }
-
   
   return (
     <div>
