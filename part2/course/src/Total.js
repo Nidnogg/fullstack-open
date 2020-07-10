ce05e35a7222765750156afacc692cd4679b0209
nidnogg <henriquevt98@gmail.com>
2020-07-10 00:32:25 -0300

    reduce example working

--- part2/course/src/Total.js
+++ part2/course/src/Total.js
@@ -3,16 +3,16 @@ import React from 'react'
 const Total = (props) => {
   const { course } = props
   const parts = course.parts
-  //const total = /*parts[0].exercises + parts[1].exercises + parts[2].exercises*/
+
   const total = parts.reduce((total, cur) => {
     console.log(`what the hell is total: ${total} and cur: ${cur}`)
-    return total + cur
-  })
+    return total + cur.exercises
+  }, 0)
 
   return (
-    <p>
-      {total}
-    </p>
+    <h3>
+      total of {total} exercises
+    </h3>
   )
 }
 
